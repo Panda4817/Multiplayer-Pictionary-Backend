a3045db3e71b08bcaeddc5a92ec0da87ef3cfd02
kanta <kanta.munton@gmail.com>
2020-09-28 22:47:00 +0100

    server set up to handle users

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const socketio = require('socket.io');
+const http = require('http');
+
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./users')
+
+const PORT = process.env.PORT || 5000
+
+const router = require('./router');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+io.on('connection', (socket) => {
+    console.log('We have a new connection');
+
+    socket.on('join', ({ name, room }, callback) => {
+        console.log(name, room)
+        let { error, user } = addUser({ id: socket.id, name, room });
+
+        if (error) return callback(error);
+        
+        socket.join(user.room).emit();
+        let list = getUsersInRoom(room)
+        console.log(list)
+        socket.emit('updateUsers', list);
+        socket.broadcast.to(user.room).emit('updateUsers', list);
+        
+    });
+
+    socket.on('changeWaiting', (room) => {
+        socket.broadcast.to(room).emit('waitingFalse');
+    })
+
+    socket.on('disconnect', () => {
+        console.log('User has left');
+        removeUser(socket.id);
+    })
+});
+
+app.use(router);
+
+server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
+
