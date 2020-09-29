17cd25d28d374c62c87abe4804a9714d1ab7ac54
kanta <kanta.munton@gmail.com>
2020-09-29 06:00:12 +0100

    added words list and random room name

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ const PORT = process.env.PORT || 5000
 const router = require('./router');
 
 const app = express();
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    next();
+});
 const server = http.createServer(app);
 const io = socketio(server);
 
