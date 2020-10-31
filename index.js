99036e69c947435bfb3265b1f02546956bad51b8
kanta <kanta.munton@gmail.com>
2020-10-31 22:01:05 +0000

    update cors

--- index.js
+++ index.js
@@ -221,7 +221,7 @@ io.on('connection', (socket) => {
 
 app.use(router)
 app.options("/*", function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Origin', 'https://picto.netlify.app')
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With')
     res.sendStatus(200)
